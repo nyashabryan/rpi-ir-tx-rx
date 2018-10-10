07bd6c0b927cf99f34150af112b7823912df2dfc
Nyasha Bryan <nyashabryan23@gmail.com>
2018-10-10 15:43:32 +0200

    Finnessed that TX

--- tx.py
+++ tx.py
@@ -1,6 +1,10 @@
+#!usr/bin/python
+
+from socket import *
 import pigpio
 import queue
 import time
+import threading
 
 PWM = 18
 OUTPUT = 14
@@ -8,13 +12,21 @@ DATA_OUT = 0
 
 FREQ = 2900  # In bits/sec
 
+serTCPsock = socket(AF_INET, SOCK_STREAM)
+HOST = ''
+PORT = 5005
+BUFSIZE = 1024
+ADDR = (HOST,PORT)
+
+# Start the pi object
+pi = pigpio.pi()
+
 # receive from socket.and put into a queue
-main_queue =  queue.Queue()
-main_queue.put("101001110110010000")
+MAIN_QUEUE =  queue.Queue()
+MAIN_QUEUE.put("101001110110010000")
 # once queue is set, send the queue
 
 # start pwm
-
 def calculate_parity(HEADER, TAIL, bitstream):
 	"""
 		Calculates the value of the even parity
@@ -31,16 +43,12 @@ def make_message(bitstream):
 	HEADER = [0, 1]
 	TAIL = [0, 1]
 	bitstream = [0,1,1,0,1,0,0,1,1,1,0,1,1,0,0,1,0,0,0,0,1,0,1,0,1,0,1,0]
-	parity = calculate_parity(HEADER, TAIL, bits)
-	return bits
+	parity = calculate_parity(HEADER, TAIL, bitstream)
+	return HEADER + bitstream.append(parity) + TAIL
 
 
-def transmit(value):
-	global pi
-	bits = make_message(value)
-	print("Starting TX")
-	print(bits)
-	for bit in bits:
+def tx(bitstream):
+	for bit in bitstream:
 		if bit == 0:
 			tick = time.monotonic()
 			pi.write(OUTPUT, 1)
@@ -63,8 +71,14 @@ def transmit(value):
 	print("Done Transmitting")
 
 
+def transmit(value):
+	bitstream = make_message(value)
+	print("Starting TX")
+	print(bitstream)
+	tx(bitstream)
 
-def main():
+
+def IR_TX():
 	global pi
 	try:
 		time.sleep(5)
@@ -76,59 +90,47 @@ def main():
 
 		pi.set_mode(OUTPUT, pigpio.OUTPUT)
 		time.sleep(2)
-		value = "101010111110000111"
-		transmit(value)
+		print("IR TX set up")
 		while(True):
-			pass
+			if MAIN_QUEUE.not_empty:
+				transmit(MAIN_QUEUE.get())
+			
 		pi.stop()
-
-
-
 	except KeyboardInterrupt:
 		pi.stop()
+		exit()
 
-main()
-pi.stop()
-"""
-#!usr/bin/python
 
-#Communicating with Android App
-#Michael Granelli
-#5 September 2018
-
-from socket import *
-import time
-
-HOST = ''
-PORT = 5005
-BUFSIZE = 1024
-ADDR = (HOST,PORT)
-
-serTCPsock = socket(AF_INET, SOCK_STREAM)
-serTCPsock.bind(ADDR)
-serTCPsock.listen(5)
-cont=1
-
-while 1:
-	print 'Waiting to be connected'
-	cliTCPsock,addr =serTCPsock.accept()
-	print 'Connected to:',addr
+def WIFI_RX():
 	try:
-		while 1:
+		serTCPsock.bind(ADDR)
+		serTCPsock.listen(5)
+		cliTCPsock,addr =serTCPsock.accept()
+		print("WIFI set up at ", addr)
+		while(True):
 			data = ''
 			data = cliTCPsock.recv(BUFSIZE)
 			if not data:
-				break
-			if data!='quit':
-				print data
+				continue
+			elif data != 'quit':
+				MAIN_QUEUE.put(data)
 			elif data=='quit':
 				serTCPsock.close()
 				exit()
-				break
+
 	except KeyboardInterrupt:
-		break
-serTCPsock.close()
+		serTCPsock.close()
+		exit()
 
 
+def main():
+	threads = [
+		threading.Thread(target=IR_TX, args=(MAIN_QUEUE)),
+		threading.Thread(target=WIFI_RX, args=(MAIN_QUEUE))
+	]
+	for thread in threads:
+		thread.start()
 
-"""
+if __name__ == "__main__":
+	main()
+	pi.stop()
