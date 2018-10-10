8c0426c0f9005d31f004ec60e636ff7afa131b7d
Nyasha Bryan <nyashabryan23@gmail.com>
2018-10-10 16:25:57 +0200

    Finesse that RX module

--- receive.py
+++ receive.py
@@ -1,11 +1,14 @@
 import pigpio
 import time
+import queue
 
 INPUT = 3
 FREQ = 10
 OUT = []
 
 pi = pigpio.pi()
+MAIN_QUEUE = queue.Queue()
+LAST_DATA = 0
 
 TRANSMITTING = False
 DATA = False
@@ -13,7 +16,6 @@ LAST_TICK = 0
 ZERO_T = 0
 
 def get_values(gpio, level, tick):
-	tic = time.monotonic()
 	global TRANSMITTING, ZERO_T, LAST_TICK, DATA
 	if not TRANSMITTING:
 		TRANSMITTING = True
@@ -30,13 +32,40 @@ def get_values(gpio, level, tick):
 			print(ZERO_T)
 	LAST_TICK = tick
 
+
+def parity_check(bitstream):
+	count = 0
+	for bit in bitstream:
+		if bit == 1:
+			count = count + 1
+	return bitstream[26] == count % 2
+
+
+def process(bitstream):
+	if bitstream[0] != 0 or bitstream[1] != 1:
+		print("Header corrupt. Data discarded.")
+	if bitstream[27] != 0 or bitstream[28] != 1:
+		print("Tail corrupt. Data discarded.")
+	if parity_check(bitstream):
+		LAST_DATA = bitstream
+		return bitstream
+	else:
+		print("Data corrupt.")
+	return LAST_DATA
+
 try:
 	pi.set_mode(INPUT, pigpio.INPUT)
 	pi.set_pull_up_down(INPUT, pigpio.PUD_UP)
 	c1 = pi.callback(INPUT, pigpio.FALLING_EDGE, get_values)
-	while(len(OUT)< 20):
-		time.sleep(0.5)
-	print(OUT)
+	while(True):
+		if(len(OUT) < 29):
+			pass
+		else:
+			c1.cancel()
+			MAIN_QUEUE.join(process(OUT))
+			TRANSMITTING = False
+			DATA = False
+			c1 = pi.callback(INPUT, pigpio.FALLING_EDGE, get_values)
 	pi.stop()
 except KeyboardInterrupt:
 	pi.stop()
